31171b07f1ceedd2433bc25953c89a297acddea1
AllisterRutledge <104733431+AllisterRutledge@users.noreply.github.com>
2023-05-08 12:59:43 -0500

    Add files via upload

--- 100 days of code bootcamp/16. OOP/main.py
+++ 100 days of code bootcamp/16. OOP/main.py
@@ -1,1 +1,26 @@
+# import turtle
+#
+# # creating objects from classes Turtle() and Screen()
+# timmy = turtle.Turtle()
+# print(timmy)
+#
+# my_screen = turtle.Screen()
+#
+# # printing attribute (canvheight) of my_screen object
+# print(my_screen.canvheight)
+#
+# # using methods on timmy
+# timmy.shape("turtle")
+# timmy.color("DarkGreen")
+# timmy.forward(100)
+#
+# # calling object method for my_screen
+# my_screen.exitonclick()
 
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
