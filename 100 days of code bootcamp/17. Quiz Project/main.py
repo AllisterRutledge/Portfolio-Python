40352e12ecc3b521942200f7f9b4cd74c021acc1
AllisterRutledge <104733431+AllisterRutledge@users.noreply.github.com>
2023-05-08 13:03:30 -0500

    Add files via upload

--- 100 days of code bootcamp/17. Quiz Project/main.py
+++ 100 days of code bootcamp/17. Quiz Project/main.py
@@ -1,1 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
+question_bank = []
+for entry in question_data:
+    question = Question(entry["question"], entry["correct_answer"])
+    question_bank.append(question)
+# print(question_bank[0].text)
+# print(question_bank[0].answer)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
